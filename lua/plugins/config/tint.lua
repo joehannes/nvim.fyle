5b4f91a235173f2fa6f420bac0a0153ba1322a2a
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2023-07-14 08:37:18 -0400

    chore(cfg): tint fine tuning

--- lua/plugins/config/tint.lua
+++ lua/plugins/config/tint.lua
@@ -4,14 +4,15 @@ function M.setup()
   local transforms = require("tint.transforms")
 
   tint.setup({
-    tint = -45, -- Darken colors, use a positive value to brighten
-    saturation = 0.6, -- Saturation to preserve
+    tint = vim.opt.background:get() == "light" and 21 or -21, -- Darken colors, use a positive value to brighten
+    saturation = 0.7,                                         -- Saturation to preserve
     transforms = {
-      -- transforms.saturate(0.5),
-      transforms.tint_with_threshold(-100, "#000000", 100)
-    }, -- Showing default behavior, but value here can be predefined set of transforms
+      transforms.saturate(0.7),
+      -- transforms.tint_with_threshold(-100, "#000000", 100)
+    },                             -- Showing default behavior, but value here can be predefined set of transforms
     tint_background_colors = true, -- Tint background portions of highlight groups
-    highlight_ignore_patterns = { "WinSeparator", "StatusLine", "StatusLineNC", "WinBar",
+    highlight_ignore_patterns = { "WinSeparator", "StatusLine", "StatusLineNC", "WinBar", "Trouble", "Nofile", "nofile",
+      "Outline", "SymbolsOutline",
       "WinBarNC", "HeirLine" },
     window_ignore_function = function(winid)
       local bufid = vim.api.nvim_win_get_buf(winid)
@@ -19,7 +20,7 @@ function M.setup()
       local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
 
       -- Do not tint `terminal` or floating windows, tint everything else
-      return buftype == "terminal" or floating
+      return buftype == "terminal" or buftype == "nofile" or floating
     end
   })
 end
