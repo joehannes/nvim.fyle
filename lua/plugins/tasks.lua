53e13b4fe429e13f8964abad4d7b3c5a58cb98d6
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2023-07-14 08:38:36 -0400

    feat(taskrunner): overseer instead of asyncrun

--- lua/plugins/tasks.lua
+++ lua/plugins/tasks.lua
@@ -1,9 +1,16 @@
 return {
-  { "skywind3000/asyncrun.vim" },
   {
-    "skywind3000/asynctasks.vim",
-    dependencies = { "skywind3000/asyncrun.vim" },
+    "stevearc/overseer.nvim",
+    opts = {},
+    config = function()
+      require("overseer").setup()
+    end
   },
+  -- { "skywind3000/asyncrun.vim" },
+  -- {
+  --   "skywind3000/asynctasks.vim",
+  --   dependencies = { "skywind3000/asyncrun.vim" },
+  -- },
   {
     "michaelb/sniprun",
     build = "bash ./install.sh",
