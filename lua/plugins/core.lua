531c8b500bd2184085ca16fc1baa84dcbc58c465
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2023-12-04 10:13:06 -0400

    chore(session): silence please

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -43,7 +43,7 @@ return {
     config = function()
       require("persisted").setup({
         save_dir = vim.fn.expand(vim.fn.stdpath("data") .. "/sessions/"), -- directory where session files are saved
-        silent = false,                                                   -- silent nvim message when sourcing session file
+        silent = true,                                                    -- silent nvim message when sourcing session file
         use_git_branch = true,                                            -- create session files based on the branch of the git enabled repository
         autosave = true,                                                  -- automatically save session files when exiting Neovim
         should_autosave = nil,                                            -- function to determine if a session should be autosaved
